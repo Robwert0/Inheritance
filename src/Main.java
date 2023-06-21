e939f264bb2c1d07a3d9aa8624d4bbcc17935a0d
Robert <mirearobert32@gmail.com>
2023-06-21 16:06:23 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee robert = new Employee("Robert", "12/01/2002", 120102, "22/06/2023");
     }
 }
