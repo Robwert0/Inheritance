92a03c2125a4f30fbe7e4fc97091201f986a5407
Robert <mirearobert32@gmail.com>
2023-06-21 16:49:22 +0300

    Final touches

--- src/SalariedEmployee.java
+++ src/SalariedEmployee.java
@@ -1,5 +1,5 @@
 public class SalariedEmployee extends Employee{
-    private double annualSalary;
+    private final double annualSalary;
     private boolean isRetired;
 
     public SalariedEmployee(String name, String birthDate, String hireDate, double annualSalary) {
