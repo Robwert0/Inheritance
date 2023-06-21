ab42bc422fd21a93e8eaf8eb6d535a95585d2389
Robert <mirearobert32@gmail.com>
2023-06-21 16:45:04 +0300

    Project Finished

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,20 @@ public class Main {
         System.out.println("Age = " + robert.getAge());
         System.out.println("Pay = " + robert.collectPay());
 
-        Employee denisa = new Employee("Denisa", "01/09/2001", "30/06/2023");
+        System.out.println();
+
+        SalariedEmployee denisa = new SalariedEmployee("Denisa", "01/09/2001", "30/06/2023", 30000);
         System.out.println(denisa);
-        System.out.println("Age = " + denisa.getAge());
-        System.out.println("Pay = " + denisa.collectPay());
+        System.out.println("Denisa paycheck = $" + denisa.collectPay());
+
+        denisa.retire();
+        System.out.println("Denisa's Pension chekc = "+ denisa.collectPay() );
+
+        System.out.println();
+
+        HourlyEmployee berry = new HourlyEmployee("Berry", "08/06/2000", "03/03/2021", 15);
+        System.out.println(berry);
+        System.out.println("Berry's paycheck = $" + berry.collectPay());
+        System.out.println("berry's Holiday Pay = $" + berry.getDoublePay() );
     }
 }
